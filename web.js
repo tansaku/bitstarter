1eef197744c000fe23468ba08c113eb8c9148792
Ubuntu <ubuntu@ip-10-154-188-171.ec2.internal>
2013-07-12 14:09:59 +0000

    trying to load file

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var contents = fs.readFileSync('index.html');
+  response.send(contents);
 });
 
 var port = process.env.PORT || 5000;
